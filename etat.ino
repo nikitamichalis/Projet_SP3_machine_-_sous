87a8df3f39f1790c32168b97c481f3d3dfb5e85f
nikitamichalis <95724001+nikitamichalis@users.noreply.github.com>
2021-12-08 01:10:43 +0100

    Update etat.ino

--- etat.ino
+++ etat.ino
@@ -1,14 +1,15 @@
-
+#include <Servo.h>
+Servo monServo;
 
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 #define MAX_NUM_TRIES 3
 
+int PositionDuServo = 0;
 const byte pinButton1 = 6;
 const byte pinButton2 = 13;
 const byte pinButton3 = 8;
-int pinBuzzer = 10;
 int pinLampe = 9;
 int ButtonValue1 = 0;
 int ButtonValue2 = 0;
@@ -31,7 +32,6 @@ enum
 void mettreAuRepos()
 {
   digitalWrite(pinLampe, LOW);
-  digitalWrite(pinBuzzer, LOW);
   ButtonValue1 = 0;
   ButtonValue2 = 0;
   ButtonValue3 = 0;
@@ -45,8 +45,9 @@ void setup()
   pinMode(pinButton1, INPUT_PULLUP);
   pinMode(pinButton2, INPUT_PULLUP);
   pinMode(pinButton3, INPUT_PULLUP);
-  pinMode(pinBuzzer, OUTPUT);
   pinMode(pinLampe, OUTPUT);
+  monServo.attach(10);
+  monServo.write(PositionDuServo);
   randomSeed(analogRead(A0));
   nbRandom1 = 0;
   nbRandom2 = 0;
@@ -61,8 +62,19 @@ void loop()
   {
   case REPOS:
     ButtonValue1 = digitalRead(pinButton1);
-    if (ButtonValue1 != 0)
+    if (digitalRead(pinButton1) != HIGH)
     {
+      for (PositionDuServo = 15; PositionDuServo <= 180; PositionDuServo++)
+      {
+        monServo.write(PositionDuServo);
+        delay(15);
+      }
+      for (PositionDuServo = 180; PositionDuServo >= 15; PositionDuServo--)
+      {
+        monServo.write(PositionDuServo);
+        delay(15);
+      }
+
       for (int i = 0; i < 1; i++)
       {
         nbRandom1 = random(1, 10);
@@ -76,7 +88,8 @@ void loop()
 
   case ETAT_1:
     ButtonValue2 = digitalRead(pinButton2);
-    if (ButtonValue2 != 0)
+
+    if (digitalRead(pinButton2) != HIGH)
     {
       for (int i = 0; i < 1; i++)
       {
@@ -91,7 +104,7 @@ void loop()
 
   case ETAT_2:
     ButtonValue3 = digitalRead(pinButton3);
-    if (ButtonValue3 != 0)
+    if (digitalRead(pinButton3) != HIGH)
     {
       for (int i = 0; i < 1; i++)
       {
@@ -111,27 +124,26 @@ void loop()
     ref3 = nbRandom3;
     turn_counter++;
     if (ref1 != 0 && ref2 != 0 && ref3 != 0)
-    {
-      if (nbRandom1 != nbRandom2 && nbRandom2 != nbRandom3)
-      {
-        lcd.setCursor(1, 2);
-        lcd.print("GAME OVER");
-        digitalWrite(pinLampe, HIGH);
-      }
-      else if (nbRandom1 == nbRandom2 && nbRandom1 == nbRandom3)
+    { 
+      if (nbRandom1 == nbRandom2 && nbRandom1 == nbRandom3)
       {
         lcd.setCursor(1, 2);
         lcd.print("WINNER");
         digitalWrite(pinLampe, HIGH);
-        digitalWrite(pinBuzzer, HIGH);
-        delay(300);
-        digitalWrite(pinBuzzer, LOW);
-        delay(2000);
+        turn_counter =3;
+      }
+      else{
+        if (turn_counter >= MAX_NUM_TRIES){
+          lcd.setCursor(1, 2);
+          lcd.print("GAME OVER");
+          digitalWrite(pinLampe, HIGH);
+        }
       }
     }
     if (turn_counter >= MAX_NUM_TRIES)
     {
       // 3rd turn => Game finished
+      turn_counter=0;
       mettreAuRepos();
       etatCourant = ETAT_0;
     }
@@ -146,14 +158,9 @@ void loop()
     break;
   case ETAT_0:
     // Waiting for all buttons to be pressed together
-    while (!(ButtonValue1 && ButtonValue2 && ButtonValue3))
+    while (!(digitalRead(pinButton1) != HIGH && digitalRead(pinButton1) != HIGH && digitalRead(pinButton3) != HIGH))
     {
-      ButtonValue1 = 0;
-      ButtonValue2 = 0;
-      ButtonValue3 = 0;
-      ButtonValue1 = digitalRead(pinButton1);
-      ButtonValue2 = digitalRead(pinButton2);
-      ButtonValue3 = digitalRead(pinButton3);
+      ;
     }
     etatCourant = REPOS;
   }
